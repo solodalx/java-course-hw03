4cc5b20072e2656dc921e3687647ddaa090bb37c
Alexey <a@a.ru>
2017-11-21 00:18:30 +0300

    Commit02

--- src/sgu/Solution.java
+++ src/sgu/Solution.java
@@ -1,4 +1,24 @@
 package sgu;
 
+import java.util.*;
+
+// Задача 2057 с http://acm.sgu.ru/lang/
 public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int numberOfOperations = scanner.nextInt();
+
+//        List<Integer> list = new ArrayList<>();
+        Queue<Integer> elements = new PriorityQueue<>();
+        for (int i = 0; i < numberOfOperations; i++) {
+            int operation = scanner.nextInt();
+            if (operation == 1) {
+                elements.add(scanner.nextInt());
+            } else if (operation == 2) {
+                System.out.println(elements.poll());
+            }
+        }
+    }
+
 }
